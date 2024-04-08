08cfc754e62ab6ee83f8c92b195b02f845cc64d7
Dilum Balaarachchi <dilumbal@Dilums-MacBook-Pro.local>
2024-04-08 17:10:06 +1000

    4.1P - log function implemented

--- calculator-microservice/server.js
+++ calculator-microservice/server.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
+const winston = require("winston");
 
 app.use(express.json());
+
+function logMessage(level, message) {
+  logger.log({
+    level: level,
+    message: message,
+  });
+}
+
+function logOperationRequest(level, operation, num1, num2) {
+  logger.log({
+    level: level,
+    message: `New ${operation} operation requested: ${num1} ${operation} ${num2}`,
+  });
+}
 
 // Validation function
 function validateInput(req, res, next) {
@@ -14,16 +29,17 @@ function validateInput(req, res, next) {
     inputnum1 === "" ||
     inputnum2 === ""
   ) {
+    logMessage("error", `null error found, Both numbers are required`);
     return res.status(400).json({ error: "Both numbers are required." });
   }
 
   // Check if num1 and num2 are numbers
   if (isNaN(inputnum1) || isNaN(inputnum2)) {
+    logMessage("error", `Invalid input. Please provide valid numbers`);
     return res
       .status(400)
-      .json({ error: "Invalid input. Please provide valid numbers." });
+      .json({ error: "Invalid input. Please provide valid numbers" });
   }
-
   // If input is valid, proceed to the next middleware/route handler
   next();
 }
@@ -31,36 +47,75 @@ function validateInput(req, res, next) {
 // Function for addition
 function fnaddition(req, res) {
   const { inputnum1, inputnum2 } = req.body;
-  const result = parseFloat(inputnum1) + parseFloat(inputnum2);
-  res.json({ result });
+  // Log the addition operation request
+  logOperationRequest("info", "+", inputnum1, inputnum2);
+  // Call the validateInput function
+  validateInput(req, res, () => {
+    // Validation passed, proceed with addition operation
+    const result = parseFloat(inputnum1) + parseFloat(inputnum2);
+    res.json({ result });
+  });
 }
 
-// Function for subtraction
+// Function for substraction
 function fnsubtraction(req, res) {
   const { inputnum1, inputnum2 } = req.body;
-  const result = parseFloat(inputnum1) - parseFloat(inputnum2);
-  res.json({ result });
+  // Log the addition operation request
+  logOperationRequest("info", "-", inputnum1, inputnum2);
+  // Call the validateInput function
+  validateInput(req, res, () => {
+    // Validation passed, proceed with addition operation
+    const result = parseFloat(inputnum1) - parseFloat(inputnum2);
+    res.json({ result });
+  });
 }
 
-// Function for multiplication
-function fnmultiplication(req, res) {
-  const { inputnum1, inputnum2 } = req.body;
-  const result = parseFloat(inputnum1) * parseFloat(inputnum2);
-  res.json({ result });
-}
-
-// Function for division
+// Function for divide
 function fndivision(req, res) {
   const { inputnum1, inputnum2 } = req.body;
-  const result = parseFloat(inputnum1) / parseFloat(inputnum2);
-  res.json({ result });
+  // Log the addition operation request
+  logOperationRequest("info", "/", inputnum1, inputnum2);
+  // Call the validateInput function
+  validateInput(req, res, () => {
+    // Validation passed, proceed with addition operation
+    const result = parseFloat(inputnum1) / parseFloat(inputnum2);
+    res.json({ result });
+  });
 }
 
+// Function for multipication
+function fnmultiplication(req, res) {
+  const { inputnum1, inputnum2 } = req.body;
+  // Log the addition operation request
+  logOperationRequest("info", "x", inputnum1, inputnum2);
+  // Call the validateInput function
+  validateInput(req, res, () => {
+    // Validation passed, proceed with addition operation
+    const result = parseFloat(inputnum1) * parseFloat(inputnum2);
+    res.json({ result });
+  });
+}
+
+
+
 // API endpoints with middleware for input validation
-app.post("/api/add", validateInput, fnaddition);
-app.post("/api/substract", validateInput, fnsubtraction);
-app.post("/api/multipy", validateInput, fnmultiplication);
-app.post("/api/divide", validateInput, fndivision);
+app.post("/api/add", fnaddition);
+app.post("/api/substract", fnsubtraction);
+app.post("/api/multipy", fnmultiplication);
+app.post("/api/divide", fndivision);
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "calculator-microservice" },
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
+  ],
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
